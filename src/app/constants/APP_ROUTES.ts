f0304f2275bbccc4840ea97da7899f4918e9c362
Hitesh Patel <freelance.ionic@gmail.com>
2021-06-18 12:47:55 +0530

    Common modification in structure

---
+++ src/app/constants/APP_ROUTES.ts
@@ -0,0 +1,12 @@
+export const APP_ROUTES : any = {
+    LANDING: 'landing',
+    UPLOAD_PHOTOS: 'upload-photos',
+    REGISTER: 'register',
+    LOGIN: 'login',
+    PROFILE: 'profile',
+    VIDEO: 'job',
+    CREATE_VIDEO: 'video',
+    VIDEO_OPTIONS: 'video-options',
+    PROFILE_PHOTOS: 'profile-photos',
+    FORGOT_PASSWORD: 'forgot-password',
+};
